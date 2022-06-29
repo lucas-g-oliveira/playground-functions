2d86b7c7e5235859c81f3fd9f72e84d68dc0e50e
Lucas G <lucasg.devmail93@gmail.com>
2022-06-28 21:44:51 -0300

    11 - generatedPhoneNumber -> OK

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,32 @@
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(arrayNumbers = []) {
   // seu código aqui
+  let numberSetted = '';
+
+  let charsSpecials = { '0': '(', '2': ') ', '7': '-' };
+
+  // array.lenght === 11;
+  if (arrayNumbers.length != 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  //value > 0 e < que 9 e repeticao do numero < 3
+  for (value of arrayNumbers) {
+    let contTemp = 0;
+    for (number of arrayNumbers) {
+      contTemp += (value === number) ? 1: 0;
+    }
+  
+    if (value < 0 || value > 9 || contTemp >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  // insert SpecialsCharacter
+  for (index in arrayNumbers) {
+    numberSetted += (charsSpecials[index] != undefined) ? (charsSpecials[index] + arrayNumbers[index]) : arrayNumbers[index];
+  }
+
+  return numberSetted;
 }
 
 // Desafio 12
